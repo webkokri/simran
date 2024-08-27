f6b4f56f544cd2f15c6c78a14cc557fb409bd798
webkokri <kokriweb@yahoo.com>
2024-08-26 20:53:53 -0500

    Error fixed.

--- pages/privacy.js
+++ pages/privacy.js
@@ -29,7 +29,7 @@ function Privacy() {
                                 
 <p>Last updated: July 23, 2024</p>
 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-<p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.
+<p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
 <h2>Interpretation and Definitions</h2>
 <h3>Interpretation</h3>
 <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
@@ -207,7 +207,7 @@ function Privacy() {
 <p>By email: support@siman.ca</p>
 </li>
 <li>
-<p>By visiting this page on our website: <Link href="/contact-us" rel="external nofollow noopener" target="_blank"><a>http://siman.ca/contact-us</a></link></p>
+<p>By visiting this page on our website: <Link href="/contact-us" rel="external nofollow noopener" target="_blank"><a>http://siman.ca/contact-us</a></Link></p>
 </li>
 </ul>
 
